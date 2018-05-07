9e8b6649bb825cf3c4370b21fd12b57ec8938f2f
Matt Wynne <matt@mattwynne.net>
2018-05-07 12:56:34 +0000

    Add a fake UI

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -2,39 +2,7 @@
 
 const assert = require('assert')
 const { Before, After, Given, When, Then } = require('cucumber')
-const buildPlayer = require('../../lib/player/build')
-const buildUi = require('../../lib/ui/build')
-
-class Radio {
-  constructor({ player }) {
-    this._player = player
-    this._stationUrls = []
-  }
-
-  setStationUrls(stationUrls) {
-    this._stationUrls = stationUrls
-  }
-
-  on() {
-    const stationUrl = this._stationUrls[0]
-    this._player.play(stationUrl)
-  }
-}
-
-const buildRadio = (env) => {
-  const config = {
-    player: {
-      type: env.player_type || 'fake_player',
-      debug: false
-    },
-    ui: {
-      type: env.ui_type || 'fake_ui',
-    }
-  }
-  const dependencies = { ...buildPlayer(config.player), ...buildUi(config.ui) }
-  const radio = new Radio(dependencies)
-  return { radio, ...dependencies }
-}
+const buildRadio = require('../../lib/build')
 
 const stations = require('../../fixtures/station_urls')
 
