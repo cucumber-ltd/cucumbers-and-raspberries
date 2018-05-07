f0b32abf830b64e7b427f94fbff339962e17673e
Matt Wynne <matt@mattwynne.net>
2018-05-07 14:08:10 +0000

    Add script to start the radio manually

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const Gpio = require('onoff').Gpio
-const button = new Gpio(4, 'in', 'falling', { debounceTimeout: 10 })
+'use strict'
 
-button.watch(() => {
-  console.log('pressed!')
-})
+const buildRadio = require('./lib/build')
+
+const { radio } = buildRadio(process.env)
+radio.setStationUrls([
+  "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_p",
+  "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_6music_mf_p"
+])
+radio.on()
+console.log('The radio is playing. Press any key to exit')
+process.stdin.setRawMode(true)
+process.stdin.resume()
+process.stdin.on('data', process.exit.bind(process, 0))
