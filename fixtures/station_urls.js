838a7ec9f27c03efca0370112c8b369485d121bc
Matt Wynne <matt@mattwynne.net>
2018-05-19 16:39:58 +0000

    Add Fip URL

--- fixtures/station_urls.js
+++ fixtures/station_urls.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   "BBC Radio 4": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_p",
-  "BBC Radio 6 Music": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_6music_mf_p"
+  "BBC Radio 6 Music": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_6music_mf_p",
+  "Fip": "http://audio.scdn.arkena.com/11016/fip-midfi128.mp3"
 }
