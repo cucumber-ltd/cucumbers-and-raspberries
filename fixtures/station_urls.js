62b32406325758bbf4ab4be112c86ad288247848
Steve Tooke <tooky@cucumber.io>
2018-05-21 11:57:53 +0100

    Update station_urls.js

--- fixtures/station_urls.js
+++ fixtures/station_urls.js
@@ -3,5 +3,5 @@
 module.exports = {
   "BBC Radio 4": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_p",
   "BBC Radio 6 Music": "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_6music_mf_p",
-  "Fip": "http://audio.scdn.arkena.com/11016/fip-midfi128.mp3"
+  "Fip": "http://direct.fipradio.fr/live/fip-midfi.mp3"
 }
